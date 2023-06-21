bb7ea72d0dd993aa5fbcabde3b2e20939f41ed96
Kerrod <137239436+kwts90@users.noreply.github.com>
2023-06-21 10:59:02 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
-streamlit.list('Omega 3 & BlueBerry','Kale, Spinach & Rocket', 'Hard Boiled Egg')
+streamlit.text('Omega 3 & BlueBerry','Kale, Spinach & Rocket', 'Hard Boiled Egg')
