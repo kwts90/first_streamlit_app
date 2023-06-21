a3b6dd25911a1dc1063aac935b3a5a35d70bfc93
Kerrod <137239436+kwts90@users.noreply.github.com>
2023-06-21 15:54:32 +1000

    Update streamlit_app.py
    removed the raw json
    added variability for the normalised API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 # Display fruityvice api response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 #take the json and normalise it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
